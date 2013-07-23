000f424d6038308acf3fc87d9c2233205346ddd8
Tommy Benshaul <tommybenshaul@Tommys-MacBook-Pro.local>
2013-07-23 14:27:43 +0300

    Initial Commit

---
+++ Mathcismod/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Mathcismod
+//
+//  Created by Tommy Benshaul on 7/23/13.
+//  Copyright (c) 2013 Tommy Benshaul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Mathcismod/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Mathcismod
+//
+//  Created by Tommy Benshaul on 7/23/13.
+//  Copyright (c) 2013 Tommy Benshaul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
